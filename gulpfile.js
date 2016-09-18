42c1dd2867c9fbe213d071d339acd737a44dbbc2
Fernando <fernandovillab@gmail.com>
2016-09-18 04:08:20 +0200

    Added JavaScript and all of its features.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
-require('./gulp/tasks/styles.js');
-require('./gulp/tasks/watch.js');
-require('./gulp/tasks/sprites.js');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
